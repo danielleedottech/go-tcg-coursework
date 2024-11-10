8ef72a392228ebdea883c779bcecf6201e299577
Daniel Lee <danlee1996@proton.me>
2024-11-09 23:24:37 -0700

    Add initial implementation of maps-practice module
    Created a new module for maps-practice with a basic Go program. The program initializes a map containing website names and URLs, then prints the map.

---
+++ maps-practice/go.mod
@@ -0,0 +1,3 @@
+module github.com/danielleetech/go-tcg-coursework/maps-practice
+
+go 1.23.3
---
+++ maps-practice/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	websites := map[string]string{
+		"Google":   "https://google.com",
+		"Yahoo":    "https://yahoo.com",
+		"Facebook": "https://facebook.com",
+	}
+	fmt.Println(websites)
+
+}
