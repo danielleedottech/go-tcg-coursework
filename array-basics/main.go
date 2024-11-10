4122dd5d1ffe7b4b2502f4a1d4e5c3a33e7d97d9
Daniel Lee <danlee1996@proton.me>
2024-11-09 19:25:15 -0700

    learning about appending slices

--- array-basics/main.go
+++ array-basics/main.go
@@ -2,26 +2,43 @@ package main
 
 import "fmt"
 
+//
+//func main() {
+//	var productNames = [4]string{"A book"}
+//	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+//
+//	productNames[2] = "A carpet"
+//	fmt.Println(prices)
+//	fmt.Println(productNames)
+//	fmt.Println(prices[2])
+//
+//	featuredPrices := prices[1:]
+//	featuredPrices[0] = 199.99
+//	highlightedPrices := featuredPrices[:1]
+//	fmt.Println(featuredPrices)
+//	fmt.Println(highlightedPrices)
+//	fmt.Println(prices)
+//
+//	fmt.Println(len(prices), cap(prices))
+//	fmt.Println(len(featuredPrices), cap(featuredPrices))
+//	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
+//
+//	highlightedPrices = highlightedPrices[:3]
+//	fmt.Println(highlightedPrices)
+//}
+
 func main() {
-	var productNames = [4]string{"A book"}
-	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
-
-	productNames[2] = "A carpet"
+	prices := []float64{10.99, 8.99}
 	fmt.Println(prices)
-	fmt.Println(productNames)
-	fmt.Println(prices[2])
+	fmt.Println(prices[0:1])
 
-	featuredPrices := prices[1:]
-	featuredPrices[0] = 199.99
-	highlightedPrices := featuredPrices[:1]
-	fmt.Println(featuredPrices)
-	fmt.Println(highlightedPrices)
+	prices[1] = 9.99
+	//prices[2] = 2.99 raises error because of cap limit
+
+	updatedPrices := append(prices, 5.99)
 	fmt.Println(prices)
+	fmt.Println(updatedPrices)
 
-	fmt.Println(len(prices), cap(prices))
-	fmt.Println(len(featuredPrices), cap(featuredPrices))
-	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
-
-	highlightedPrices = highlightedPrices[:3]
-	fmt.Println(highlightedPrices)
+	updatedPrices = updatedPrices[1:]
+	fmt.Println(updatedPrices)
 }
